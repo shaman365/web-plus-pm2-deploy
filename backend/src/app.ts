492c47f37532810094266f045b1a0d6aece6d5f5
Sergey Divaev <divaev.sergey@gmail.com>
2024-07-03 19:09:36 +0300

    Включен CORS в бэке; исправлен URL API; дополнен скрипт деплоя фронтенда

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -14,13 +14,19 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('ok'));
