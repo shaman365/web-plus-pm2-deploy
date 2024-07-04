910409d2f81ac0e106961ba952c621c93727de4b
Sergey Divaev <divaev.sergey@gmail.com>
2024-07-04 16:42:08 +0300

    Добавлены скрипты конфигрурации pm2

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
